ea856c01f6b5dc2d57c655b70758a9cdafbbfbba
Asensio0227 <mtheesikhosana@gmail.com>
2023-04-04 21:00:14 +0200

    hg

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -16,7 +16,7 @@ const VideoPlayer = ({ videoId }) => {
         controls
         autoPlay
       >
-        <source src={`https://stream-video-hokn.onrender.com/videos/${videoId}`} type="video/mp4" />
+        <source src={`https://video-stream-6k52.onrender.com/videos/${videoId}`} type="video/mp4" />
         Your browser does not support the video tag 
       </video>
   )
